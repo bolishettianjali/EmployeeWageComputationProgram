b9d6dc44fd8353fe412967189f4bc451ffd00684
bolishettianjali <bolishettianjali@gamil.com>
2021-10-11 11:25:48 +0530

    Employee is Present or Absent

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,9 +1,11 @@
 
 public class EmployeeWageComputation {
-
+	int empPresent=1;
+	int empAbsent=0;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Progam");
-
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		System.out.println(empCheck);
 	}
 
 }
