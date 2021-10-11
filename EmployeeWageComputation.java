86fbdc847cfec6c6fae90a98fc70a14a0b29f0a0
bolishettianjali <bolishettianjali@gamil.com>
2021-10-11 12:57:36 +0530

    Total hrs lessthan 100

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,28 +1,31 @@
 
 
-public class EmployeeWageComputation {
+public class Emp {
+	static int workdayPerMon = 20;
+	static int fullDayHr = 8;
+	static int ParttimeHrs = 4;
+	static int totalworkhrs;
 	static int wagePerHr = 20;
-	static int fullDayHr=8;
-	static int partTimeHr=4;
-	static int workDayPerMon=20;
-	static int empCheck;
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Progam");
-		for(int i=0;i<=workDayPerMon;i++) {
-		 empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		}
-		System.out.println("Calculating Daily Employee Wages");
-		
-		switch(empCheck) {
-		case 1:
-			System.out.println(workDayPerMon*wagePerHr*fullDayHr);
-			break;
-		case 2:
-			System.out.println(workDayPerMon*wagePerHr*partTimeHr);
-			break;
-		default:
-			System.out.println("Employee is Absent");
-		}
-	}
+	private static int empCheck;
 
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage computation");
+		for (int i = 0; i <= workdayPerMon; i++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			if (totalworkhrs + fullDayHr > 100 || totalworkhrs + ParttimeHrs > 100) {
+				totalworkhrs = 100;
+				break;
+			}
+		
+		switch (empCheck) {
+		case 1:
+			totalworkhrs += fullDayHr;
+		case 2:
+			totalworkhrs += ParttimeHrs;
+		}}
+		System.out.println(totalworkhrs);
+		System.out.println(totalworkhrs * wagePerHr);
+	
+
+}
 }
