9730905579d6a4dc4accbd88d4e44e15e0a2452c
bolishettianjali <bolishettianjali@gamil.com>
2021-10-11 11:38:20 +0530

    Calculating daily Wages

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,11 +1,17 @@
 
 public class EmployeeWageComputation {
-	int empPresent=1;
-	int empAbsent=0;
+	static int wagePerHr = 20;
+	static int fullDayHr=8;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Progam");
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		System.out.println(empCheck);
+		System.out.println("Calculating Daily Employee Wages");
+		if(empCheck==1) {
+			System.out.println(wagePerHr*fullDayHr);
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
