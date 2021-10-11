c0c78a29fa37df4e1ca2f8f0430ac0f8b201d065
bolishettianjali <bolishettianjali@gamil.com>
2021-10-11 12:50:32 +0530

    Calculating wages for 20 days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,18 +1,24 @@
 
+
 public class EmployeeWageComputation {
 	static int wagePerHr = 20;
 	static int fullDayHr=8;
 	static int partTimeHr=4;
+	static int workDayPerMon=20;
+	static int empCheck;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Progam");
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		for(int i=0;i<=workDayPerMon;i++) {
+		 empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		}
 		System.out.println("Calculating Daily Employee Wages");
+		
 		switch(empCheck) {
 		case 1:
-			System.out.println(wagePerHr*fullDayHr);
+			System.out.println(workDayPerMon*wagePerHr*fullDayHr);
 			break;
 		case 2:
-			System.out.println(wagePerHr*partTimeHr);
+			System.out.println(workDayPerMon*wagePerHr*partTimeHr);
 			break;
 		default:
 			System.out.println("Employee is Absent");
